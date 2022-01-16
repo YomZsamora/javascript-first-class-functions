6a684d93eaba6d3b454eabb6bab02b937f7755e1
yomZsamora <samora.y@adzumi.co.ke>
2022-01-16 16:16:35 +0300

    Create function that returns a named function.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Receives a callback function and calls it
-function receivesAFunction(callbackFunction) {
-    callbackFunction();
-}
+function receivesAFunction(callbackFunction) { callbackFunction(); }
+
+/* Functions can also return other functions. This is useful 
+when we want to package up a function and its environment, but when we don't want to call it just yet. */
+function returnsANamedFunction() { return function() {} }
