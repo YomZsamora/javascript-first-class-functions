0747fe1a2ffce9de0bced06936dd15a556548992
yomZsamora <samora.y@adzumi.co.ke>
2022-01-16 16:40:50 +0300

    Finalised test to check for returned anonymous function.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function receivesAFunction(callbackFunction) { callbackFunction(); }
 /* Functions can also return other functions. This is useful 
 when we want to package up a function and its environment, but when we don't want to call it just yet. */
 function returnsANamedFunction() { 
-    return function namedFunction() {
-    } 
+    return function namedFunction() {} 
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {}
 }
