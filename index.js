852a0721d73ea844721f8108444b7be859c610b8
yomZsamora <samora.y@adzumi.co.ke>
2022-01-16 16:38:40 +0300

    Finalised test to check for returned named function.

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function receivesAFunction(callbackFunction) { callbackFunction(); }
 
 /* Functions can also return other functions. This is useful 
 when we want to package up a function and its environment, but when we don't want to call it just yet. */
-function returnsANamedFunction() { return function() {} }
+function returnsANamedFunction() { 
+    return function namedFunction() {
+    } 
+}
