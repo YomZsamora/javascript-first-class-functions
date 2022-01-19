737e7d76b01acd9957764b37252f0bfc5caf39e0
yomZsamora <samora.y@adzumi.co.ke>
2022-01-19 17:05:23 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function receivesAFunction(callbackFunction) { callbackFunction(); }
 /* Functions can also return other functions. This is useful 
 when we want to package up a function and its environment, but when we don't want to call it just yet. */
 function returnsANamedFunction() { 
-    return function namedFunction() {} 
+	return function namedFunction() {} 
 }
 
 // Finalised test to check for returned Anonymous Function
 function returnsAnAnonymousFunction() {
-    return function() {}
+	return function() {}
 }
